e03092f111b8a19f7937900ec455f83cd4fd82b3
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-09-30 15:30:58 -0700

    setting up the project

---
+++ database.js
@@ -0,0 +1,45 @@
+let users = [
+	{ id: "1", name: "Jane Doe", bio: "I love to cook." },
+	{ id: "2", name: "John Doe", bio: "I love Jacob." },
+	{ id: "3", name: "Jack Doe", bio: "I dont know who those other guys are." },
+]
+
+function getUsers() {
+	return users
+}
+
+function getUserById(id) {
+	return users.find(u => u.id === id)
+}
+
+function createUser(data) {
+	const payload = {
+		id: String(users.length + 1),
+		...data,
+	}
+
+	users.push(payload)
+	return payload
+}
+
+function updateUser(id, data) {
+	const index = users.findIndex(u => u.id === id)
+	users[index] = {
+		...users[index],
+		...data,
+	}
+	
+	return users[index]
+}
+
+function deleteUser(id) {
+	users = users.filter(u => u.id != id)
+}
+
+module.exports = {
+	getUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+}
